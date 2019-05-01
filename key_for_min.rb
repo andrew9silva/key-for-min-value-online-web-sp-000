245318b6968f729872a80996c62cc85148f1513c
Andrew Silva <andrewsilva9@gmail.com>
2019-05-01 01:40:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,9 @@ def key_for_min_value(name_hash)
     if min_value == nil
       min_value = value
       min_key = key
+    elsif min_value > value
+      min_value = value
+      min_key = key
     end
   end
   min_key
