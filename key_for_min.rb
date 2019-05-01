0ba690cd04c607cb93bec6b5bf1018913cf88a12
Andrew Silva <andrewsilva9@gmail.com>
2019-05-01 01:08:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.each do |key, value|
-    if true
-      return 
+     min_value == value
+      return
     else
     end
   end
